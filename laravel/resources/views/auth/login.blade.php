8e95d2f94fcf8bc2587bb24f2b2325e09bbcbafe
HorváthKoszednárOsbáth <hkogites@gmail.com>
2025-09-23 10:11:50 +0200

    Login wooooooow

---
+++ laravel/resources/views/auth/login.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.base')
+
+@section('title', 'Bejelentkezés')
+
+@section('body')
+<div class="site-section">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h2 class="mb-4">Bejelentkezés</h2>
+
+        @if(session('status'))
+          <div class="alert alert-success">{{ session('status') }}</div>
+        @endif
+
+        @if ($errors->any())
+          <div class="alert alert-danger">
+            <ul class="mb-0">
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+
+        <form method="POST" action="{{ url('/bejelentkezes') }}">
+          @csrf
+          <div class="form-group">
+            <label for="email">E-mail cím</label>
+            <input id="email" type="email" name="email" class="form-control" value="{{ old('email') }}" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="password">Jelszó</label>
+            <input id="password" type="password" name="password" class="form-control" required>
+          </div>
+          <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="remember" name="remember">
+            <label class="form-check-label" for="remember">Emlékezz rám</label>
+          </div>
+          <div class="form-group d-flex align-items-center">
+            <button type="submit" class="btn btn-primary mr-3">Bejelentkezés</button>
+            <a href="{{ url('/regisztracio') }}">Nincs fiókja? Regisztráció</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+@endsection
+
+
---
+++ laravel/resources/views/pages/about.blade.php
@@ -0,0 +1,309 @@
+﻿<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>SmartVoyager</title>
+    <link rel="icon" type="image/x-icon" href="/images/logokicsi.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,700,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/oldal/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="/oldal/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/oldal/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/oldal/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="/oldal/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/oldal/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/oldal/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="/oldal/css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="/oldal/css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3 ">
+              <div class="site-logo">
+                <a href="index.html" class="font-weight-bold">
+                  <img src="/oldal/images/logo.png" alt="Image" class="img-fluid">
+                </a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
+
+              
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li><a href="index.html" class="nav-link">Kezdőlap</a></li>
+                  <li class="active"><a href="about.html" class="nav-link">Rólunk</a></li>
+                  <li><a href="trips.html" class="nav-link">Utazások</a></li>
+                  <li><a href="contact.html" class="nav-link">Kapcsolat</a></li>
+                  @auth
+                  <li><a href="{{ route('profile') }}" class="nav-link">Profil</a></li>
+                  <li>
+                    <form method="POST" action="{{ route('logout') }}" style="display:inline;">
+                      @csrf
+                      <button type="submit" class="nav-link p-0" style="display:inline; background:none; border:0; padding:0; font: inherit; color: inherit; cursor:pointer;">Kijelentkezés</button>
+                    </form>
+                  </li>
+                  @else
+                  <li><a href="{{ url('/bejelentkezes') }}" class="nav-link">Bejelentkezés</a></li>
+                  <li><a href="{{ url('/regisztracio') }}" class="nav-link">Regisztráció</a></li>
+                  @endauth
+
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" style="background-image: url('/oldal/images/banner.png')">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-5" data-aos="fade-up">
+              <h1 class="mb-3 text-white">About Us</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis in tenetur doloremque, maiores doloribus officia iste. Dolores.</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section py-5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6">
+            <div class="heading-39101 mb-5">
+              <span class="backdrop">Story</span>
+              <span class="subtitle-39191">Discover Story</span>
+              <h3>Our Story</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quae expedita fugiat quo incidunt, possimus temporibus aperiam eum, quaerat sapiente.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis enim a pariatur molestiae.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="/oldal/images/traveler.jpg" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section py-5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 order-2 ml-auto">
+            <div class="heading-39101 mb-5">
+              <span class="backdrop">Mission</span>
+              <span class="subtitle-39191">Our Mission</span>
+              <h3>Our Mission</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quae expedita fugiat quo incidunt, possimus temporibus aperiam eum, quaerat sapiente.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis enim a pariatur molestiae.</p>
+          </div>
+          <div class="col-md-6 order-1" data-aos="fade-left">
+            <img src="/oldal/images/banner.png" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="site-section">
+
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-10">
+            <div class="heading-39101 mb-5">
+              <span class="backdrop text-center">Our Team</span>
+              <span class="subtitle-39191">Amazing Staff</span>
+              <h3>Meet Our Team</h3>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="person-29191 text-center">
+              <img src="/oldal/images/person_1.jpg" alt="Image" class="img-fluid mb-4">
+              <div class="px-4">
+                <h2 class="mb-2">John Doe</h2>
+                <p class="caption mb-4">Staff</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, maiores? Eos alias fugit eius, repudiandae molestias error</p>
+                <div class="social_29128 mt-5">
+                <a href="#"><span class="icon-facebook"></span></a>  
+                <a href="#"><span class="icon-instagram"></span></a>  
+                <a href="#"><span class="icon-twitter"></span></a>  
+               </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="person-29191 text-center">
+              <img src="/oldal/images/person_2.jpg" alt="Image" class="img-fluid mb-4">
+              <div class="px-4">
+                <h2 class="mb-2">Jean Doe</h2>
+                <p class="caption mb-4">Staff</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, maiores? Eos alias fugit eius, repudiandae molestias error</p>
+                <div class="social_29128 mt-5">
+                <a href="#"><span class="icon-facebook"></span></a>  
+                <a href="#"><span class="icon-instagram"></span></a>  
+                <a href="#"><span class="icon-twitter"></span></a>  
+               </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="person-29191 text-center">
+              <img src="/oldal/images/person_3.jpg" alt="Image" class="img-fluid mb-4">
+              <div class="px-4">
+                <h2 class="mb-2">Claire Dormey</h2>
+                <p class="caption mb-4">Staff</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, maiores? Eos alias fugit eius, repudiandae molestias error</p>
+                <div class="social_29128 mt-5">
+                <a href="#"><span class="icon-facebook"></span></a>  
+                <a href="#"><span class="icon-instagram"></span></a>  
+                <a href="#"><span class="icon-twitter"></span></a>  
+               </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+
+    
+
+    
+
+    <div class="site-section bg-image overlay" style="background-image: url('/oldal/images/banner.png')">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-7 text-center">
+            <h2 class="font-weight-bold text-white">Join and Trip With Us</h2>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ut, doloremque quo molestiae nesciunt officiis veniam, beatae dignissimos!</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary text-white py-3 px-4">Get In Touch</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="site-footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <h2 class="footer-heading mb-3">Instagram</h2>
+            <div class="row">
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_1.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_2.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_3.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_4.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_5.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+              <div class="col-4 gal_col">
+                <a href="#"><img src="/oldal/images/insta_6.jpg" alt="Image" class="img-fluid"></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 ml-auto">
+            <div class="row">
+            <div class="col-lg-6 ml-auto">
+                <h2 class="footer-heading mb-4">Gyors elérés</h2>
+                <ul class="list-unstyled">
+                  <li><a href="index.html">Kezdőlap</a></li>
+                  <li><a href="about.html">Rólunk</a></li>
+                  <li><a href="trips.html">Utazások</a></li>
+                  <li><a href="contact.html">Kapcsolat</a></li>
+                  <li><a href="{{ url('/regisztracio') }}">Regisztráció</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <h2 class="footer-heading mb-4">Newsletter</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt odio iure animi ullam quam, deleniti rem!</p>
+                <form action="#" class="d-flex" class="subscribe">
+                  <input type="text" class="form-control mr-3" placeholder="Email">
+                  <input type="submit" value="Send" class="btn btn-primary">
+                </form>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="/oldal/js/jquery-3.3.1.min.js"></script>
+    <script src="/oldal/js/jquery-migrate-3.0.0.js"></script>
+    <script src="/oldal/js/popper.min.js"></script>
+    <script src="/oldal/js/bootstrap.min.js"></script>
+    <script src="/oldal/js/owl.carousel.min.js"></script>
+    <script src="/oldal/js/jquery.sticky.js"></script>
+    <script src="/oldal/js/jquery.waypoints.min.js"></script>
+    <script src="/oldal/js/jquery.animateNumber.min.js"></script>
+    <script src="/oldal/js/jquery.fancybox.min.js"></script>
+    <script src="/oldal/js/jquery.stellar.min.js"></script>
+    <script src="/oldal/js/jquery.easing.1.3.js"></script>
+    <script src="/oldal/js/bootstrap-datepicker.min.js"></script>
+    <script src="/oldal/js/isotope.pkgd.min.js"></script>
+    <script src="/oldal/js/aos.js"></script>
+
+    <script src="/oldal/js/main.js"></script>
+
+  </body>
+
+</html>
+
